f7b3cf59bb9a1d6138bc3146fa61ecd079055d8d
luizok <luizfelipesmaciel@gmail.com>
2021-06-08 23:08:40 -0300

    Modularizao testes

--- trabalho_1/main.c
+++ trabalho_1/main.c
@@ -7,21 +7,13 @@
 #include "src/rid.h"
 
 
-int main(int argc, char** argv) {
+void test_insert(DB_Manager_t* manager) {
 
-    if(argc < 3) {
-        printf("Usage: %s <n_pages> <n_records>\n", argv[0]);
-        return -1;
-    }
-
-    int n_pages = atoi(argv[1]);
-    int n_records = atoi(argv[2]);
-    DB_Manager_t* manager = new_dbmanager(n_pages, n_records);
+    printf("Insercaozinha top: \n");
+    __uint32_t n_pages = manager->n_pages;
+    __uint32_t n_records = manager->all_pages[0]->max_records;
     Record_t* record = NULL;
     Rid_t* rid = NULL;
-
-    print_dbmanager(manager);
-    printf("\n");
 
     for(int i=0; i < n_pages*n_records; i++) {
         record = new_record(2*i);
@@ -36,10 +28,18 @@ int main(int argc, char** argv) {
     record = new_record(2*n_pages*n_records);
     rid = insert_record(manager, record);
     if(!rid)
-    printf("%d NOT INSERTED\n", 2*n_pages*n_records);
+        printf("%d NOT INSERTED\n", 2*n_pages*n_records);
 
     print_dbmanager(manager);
     printf("\n\n");
+}
+
+void test_search(DB_Manager_t* manager) {
+
+    printf("Buscazinha top: \n");
+    __uint32_t n_pages = manager->n_pages;
+    __uint32_t n_records = manager->all_pages[0]->max_records;
+    Rid_t* rid = NULL;
 
     for(int i=0; i < 2*n_pages*n_records+1; i++) {
         rid = search_record(manager, new_record(i));
@@ -51,6 +51,12 @@ int main(int argc, char** argv) {
 
         printf("\n");
     }
+
+    printf("\n");
+}
+
+void test_scan(DB_Manager_t* manager) {
+
     printf("Scanzin top: \n");
     Record_t*** records = get_records_in_pages(manager);
     Record_t*** curr_page = records;
@@ -58,5 +64,32 @@ int main(int argc, char** argv) {
     for(curr_page; *curr_page != NULL; curr_page++)
         for(Record_t** curr_record = *curr_page; *curr_record != NULL; curr_record++)
             print_record(*curr_record);
+}
 
+void test_delete(DB_Manager_t* manager) {
+
+    printf("\nDelecaozinha top: \n");
+}
+
+int main(int argc, char** argv) {
+
+    if(argc < 3) {
+        printf("Usage: %s <n_pages> <n_records>\n", argv[0]);
+        return -1;
+    }
+
+    int n_pages = atoi(argv[1]);
+    int n_records = atoi(argv[2]);
+    DB_Manager_t* manager = new_dbmanager(n_pages, n_records);
+
+    printf("Estadozin inicial: \n");
+    print_dbmanager(manager);
+    printf("\n");
+
+    test_insert(manager);
+    test_search(manager);
+    test_scan(manager);
+    test_delete(manager);
+
+    return 0;
 }
